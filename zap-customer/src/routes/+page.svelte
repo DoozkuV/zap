cfb0b882e8b6be73a218622b718eb24993c45e20
George Padron <georgenpadron@gmail.com>
2024-07-24 17:44:20 -0400

    Full project formatting with prettier

--- zap-customer/src/routes/+page.svelte
+++ zap-customer/src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Zap</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-primary">Hello Daisy UI!</button>
--- zap-customer/tailwind.config.js
+++ zap-customer/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,svelte,js,ts}'],
-	theme: {
-		extend: {},
-		fontFamily: {
-			sans: ['League Spartan', 'sans-serif'],
-		}
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: ['bumblebee']
-	}
+  content: ["./src/**/*.{html,svelte,js,ts}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["League Spartan", "sans-serif"],
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["bumblebee"],
+  },
 };

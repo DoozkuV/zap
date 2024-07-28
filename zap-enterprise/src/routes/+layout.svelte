4c77e6feb2854364a27246801a820fcbb50be6b0
Hannah Wang <43276816+hannah0wang@users.noreply.github.com>
2024-07-27 22:07:40 -0500

    styling sidebar

--- zap-enterprise/src/routes/+layout.svelte
+++ zap-enterprise/src/routes/+layout.svelte
@@ -1,49 +1,98 @@
 <script lang="ts">
-    import '../app.css';
-    const { children } = $props(); // svelte 5 replacement for <slot />
-    let isExpanded = $state(false); // svelte 5 runes (svelte 4 version: `$: isExpanded = false;` )
-  
-    function toggleSidebar() {
-      isExpanded = !isExpanded;
-    }
-  </script>
-  
-  
-  <div class="container">
-    <div class="flex">
-      <aside 
-        class={`parent bg-gray-400 min-h-screen transition-width duration-500 ease-in-out ${isExpanded ? 'sm:w-1/5' : 'sm:w-16'}`}
-        on:mouseover={toggleSidebar}
-        on:mouseout={toggleSidebar}
-      >
-        <div class="sticky top-0 pt-12 px-2 w-full">
-          <nav>
-            <ul class="flex flex-col overflow-hidden space-y-2">
-              <li class="hover:text-gray-200 h-8">
-                <a href="#" class="flex items-center h-8">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        class="h-8 w-8 mr-3 inline-block no-transition">
-                        <path
-                        fill-rule="evenodd"
-                        d="M2.8 1L2.74967 0.99997C2.52122 0.999752 2.32429 0.999564 2.14983 1.04145C1.60136 1.17312 1.17312 1.60136 1.04145 2.14983C0.999564 2.32429 0.999752 2.52122 0.99997 2.74967L1 2.8V5.2L0.99997 5.25033C0.999752 5.47878 0.999564 5.67572 1.04145 5.85017C1.17312 6.39864 1.60136 6.82688 2.14983 6.95856C2.32429 7.00044 2.52122 7.00025 2.74967 7.00003L2.8 7H5.2L5.25033 7.00003C5.47878 7.00025 5.67572 7.00044 5.85017 6.95856C6.39864 6.82688 6.82688 6.39864 6.95856 5.85017C7.00044 5.67572 7.00025 5.47878 7.00003 5.25033L7 5.2V2.8L7.00003 2.74967C7.00025 2.52122 7.00044 2.32429 6.95856 2.14983C6.82688 1.60136 6.39864 1.17312 5.85017 1.04145C5.67572 0.999564 5.47878 0.999752 5.25033 0.99997L5.2 1H2.8ZM2.38328 2.01382C2.42632 2.00348 2.49222 2 2.8 2H5.2C5.50779 2 5.57369 2.00348 5.61672 2.01382C5.79955 2.05771 5.94229 2.20045 5.98619 2.38328C5.99652 2.42632 6 2.49222 6 2.8V5.2C6 5.50779 5.99652 5.57369 5.98619 5.61672C5.94229 5.79955 5.79955 5.94229 5.61672 5.98619C5.57369 5.99652 5.50779 6 5.2 6H2.8C2.49222 6 2.42632 5.99652 2.38328 5.98619C2.20045 5.94229 2.05771 5.79955 2.01382 5.61672C2.00348 5.57369 2 5.50779 2 5.2V2.8C2 2.49222 2.00348 2.42632 2.01382 2.38328C2.05771 2.20045 2.20045 2.05771 2.38328 2.01382ZM9.8 1L9.74967 0.99997C9.52122 0.999752 9.32429 0.999564 9.14983 1.04145C8.60136 1.17312 8.17312 1.60136 8.04145 2.14983C7.99956 2.32429 7.99975 2.52122 7.99997 2.74967L8 2.8V5.2L7.99997 5.25033C7.99975 5.47878 7.99956 5.67572 8.04145 5.85017C8.17312 6.39864 8.60136 6.82688 9.14983 6.95856C9.32429 7.00044 9.52122 7.00025 9.74967 7.00003L9.8 7H12.2L12.2503 7.00003C12.4788 7.00025 12.6757 7.00044 12.8502 6.95856C13.3986 6.82688 13.8269 6.39864 13.9586 5.85017C14.0004 5.67572 14.0003 5.47878 14 5.25033L14 5.2V2.8L14 2.74967C14.0003 2.52122 14.0004 2.32429 13.9586 2.14983C13.8269 1.60136 13.3986 1.17312 12.8502 1.04145C12.6757 0.999564 12.4788 0.999752 12.2503 0.99997L12.2 1H9.8ZM9.38328 2.01382C9.42632 2.00348 9.49222 2 9.8 2H12.2C12.5078 2 12.5737 2.00348 12.6167 2.01382C12.7995 2.05771 12.9423 2.20045 12.9862 2.38328C12.9965 2.42632 13 2.49222 13 2.8V5.2C13 5.50779 12.9965 5.57369 12.9862 5.61672C12.9423 5.79955 12.7995 5.94229 12.6167 5.98619C12.5737 5.99652 12.5078 6 12.2 6H9.8C9.49222 6 9.42632 5.99652 9.38328 5.98619C9.20045 5.94229 9.05771 5.79955 9.01382 5.61672C9.00348 5.57369 9 5.50779 9 5.2V2.8C9 2.49222 9.00348 2.42632 9.01382 2.38328C9.05771 2.20045 9.20045 2.05771 9.38328 2.01382ZM2.74967 7.99997L2.8 8H5.2L5.25033 7.99997C5.47878 7.99975 5.67572 7.99956 5.85017 8.04145C6.39864 8.17312 6.82688 8.60136 6.95856 9.14983C7.00044 9.32429 7.00025 9.52122 7.00003 9.74967L7 9.8V12.2L7.00003 12.2503C7.00025 12.4788 7.00044 12.6757 6.95856 12.8502C6.82688 13.3986 6.39864 13.8269 5.85017 13.9586C5.67572 14.0004 5.47878 14.0003 5.25033 14L5.2 14H2.8L2.74967 14C2.52122 14.0003 2.32429 14.0004 2.14983 13.9586C1.60136 13.8269 1.17312 13.3986 1.04145 12.8502C0.999564 12.6757 0.999752 12.4788 0.99997 12.2503L1 12.2V9.8L0.99997 9.74967C0.999752 9.52122 0.999564 9.32429 1.04145 9.14983C1.17312 8.60136 1.60136 8.17312 2.14983 8.04145C2.32429 7.99956 2.52122 7.99975 2.74967 7.99997ZM2.8 9C2.49222 9 2.42632 9.00348 2.38328 9.01382C2.20045 9.05771 2.05771 9.20045 2.01382 9.38328C2.00348 9.42632 2 9.49222 2 9.8V12.2C2 12.5078 2.00348 12.5737 2.01382 12.6167C2.05771 12.7995 2.20045 12.9423 2.38328 12.9862C2.42632 12.9965 2.49222 13 2.8 13H5.2C5.50779 13 5.57369 12.9965 5.61672 12.9862C5.79955 12.9423 5.94229 12.7995 5.98619 12.6167C5.99652 12.5737 6 12.5078 6 12.2V9.8C6 9.49222 5.99652 9.42632 5.98619 9.38328C5.94229 9.20045 5.79955 9.05771 5.61672 9.01382C5.57369 9.00348 5.50779 9 5.2 9H2.8ZM9.8 8L9.74967 7.99997C9.52122 7.99975 9.32429 7.99956 9.14983 8.04145C8.60136 8.17312 8.17312 8.60136 8.04145 9.14983C7.99956 9.32429 7.99975 9.52122 7.99997 9.74967L8 9.8V12.2L7.99997 12.2503C7.99975 12.4788 7.99956 12.6757 8.04145 12.8502C8.17312 13.3986 8.60136 13.8269 9.14983 13.9586C9.32429 14.0004 9.52122 14.0003 9.74967 14L9.8 14H12.2L12.2503 14C12.4788 14.0003 12.6757 14.0004 12.8502 13.9586C13.3986 13.8269 13.8269 13.3986 13.9586 12.8502C14.0004 12.6757 14.0003 12.4788 14 12.2503L14 12.2V9.8L14 9.74967C14.0003 9.52122 14.0004 9.32429 13.9586 9.14983C13.8269 8.60136 13.3986 8.17312 12.8502 8.04145C12.6757 7.99956 12.4788 7.99975 12.2503 7.99997L12.2 8H9.8ZM9.38328 9.01382C9.42632 9.00348 9.49222 9 9.8 9H12.2C12.5078 9 12.5737 9.00348 12.6167 9.01382C12.7995 9.05771 12.9423 9.20045 12.9862 9.38328C12.9965 9.42632 13 9.49222 13 9.8V12.2C13 12.5078 12.9965 12.5737 12.9862 12.6167C12.9423 12.7995 12.7995 12.9423 12.6167 12.9862C12.5737 12.9965 12.5078 13 12.2 13H9.8C9.49222 13 9.42632 12.9965 9.38328 12.9862C9.20045 12.9423 9.05771 12.7995 9.01382 12.6167C9.00348 12.5737 9 12.5078 9 12.2V9.8C9 9.49222 9.00348 9.42632 9.01382 9.38328C9.05771 9.20045 9.20045 9.05771 9.38328 9.01382Z"
-                        clip-rule="evenodd" />
-                    </svg>
-                  <span class:hidden={!isExpanded}>
-                    Dashboard
-                  </span>
-                </a>
-              </li>
-              <!-- More list items here -->
-            </ul>
-          </nav>
-        </div>
-      </aside>
-      <main class="sm:w-4/5 p-4 pt-12 px-8">
-        {@render children()}
-      </main>
-    </div>
-  </div>
-  
+  const { children } = $props();
+  let mini = $state(true);
+
+  function toggleSidebar() {
+      mini = !mini;
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap'); 
+
+  .sidebar {
+      height: 100%;
+      position: fixed;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      background-color: #cbcbcb;
+      transition: width 0.5s;
+      overflow-x: hidden;
+      padding-top: 60px;
+      white-space: nowrap;
+      font-family: 'League Spartan', sans-serif;
+  }
+  .sidebar a {
+      padding: 8px 8px 8px 32px;
+      text-decoration: none;
+      font-size: 25px;
+      color: #636363;
+      display: block;
+  }
+  .sidebar a:hover {
+      color: #f1f1f1;
+  }
+
+  .logo-box {
+    padding: 8px 8px 8px 32px;
+      text-decoration: none;
+      font-size: 25px;
+      color: #636363;
+      display: block;
+      margin-bottom: 50px;
+      margin-top: -20px;
+  }
+
+  .sidebar img {
+    width: 55px;
+    vertical-align: middle;
+    padding-bottom: 3px;
+    margin-right: 20px;
+    margin-left: -16px;
+  }
+  .material-icons {
+      vertical-align: middle;
+      padding-bottom: 3px;
+      margin-right: 30px;
+  }
+  .logo-text {
+      vertical-align: middle;
+      font-size: 40px;
+      font-weight: bold;
+      margin-left: -2px;
+  }
+  .icon-text {
+      vertical-align: middle;
+  }
+  #main {
+      padding: 16px;
+      transition: margin-left 0.5s;
+  }
+  @media screen and (max-height: 450px) {
+      .sidebar {
+          padding-top: 15px;
+      }
+      .sidebar a {
+          font-size: 18px;
+      }
+  }
+</style>
+
+<!-- Sidebar with reactive width based on the 'mini' state -->
+<div class="sidebar"
+   style:width={mini ? '85px' : '250px'}
+   on:mouseover={toggleSidebar}
+   on:mouseout={toggleSidebar}>
+  <div class="logo-box"><img src="zap-logo.png" alt="logo" class="logo"/><span class="logo-text">ZAP</span></div>
+  <a href="/"><i class="material-icons">grid_view</i><span class="icon-text">Dashboard</span></a><br>
+  <a href="/"><i class="material-icons">bolt</i><span class="icon-text">Queue</span></a><br>
+  <a href="/"><i class="material-icons">edit_document</i><span class="icon-text">Edit Menu</span></a><br>
+  <a href="/"><i class="material-icons">group</i><span class="icon-text">Customers</span></a>
+</div>
+
+<!-- Main content with reactive margin-left based on the 'mini' state -->
+<div id="main" style:margin-left={mini ? '85px' : '250px'}>
+  {@render children()}
+</div>

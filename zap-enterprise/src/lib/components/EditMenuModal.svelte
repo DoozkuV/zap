deb1ff021d89bc85e609cdd1f2edbb6de55419da
Hannah Wang <43276816+hannah0wang@users.noreply.github.com>
2024-07-29 16:38:33 -0500

    delete imgs

--- zap-enterprise/src/lib/components/EditMenuModal.svelte
+++ zap-enterprise/src/lib/components/EditMenuModal.svelte
@@ -8,10 +8,12 @@
     let showAlert = false;
     let alertMessage = "";
 
+    export let data;
+
     function handleSubmit() {
     if (validateInputs()) {
       console.log("Form is valid. saving...");
-      
+      isModalOpen = false;
       // close the modal and reset inputs after saving
       isModalOpen = false;
       setTimeout(() => {
@@ -21,7 +23,8 @@
         itemDescriptionInput = "";
       }, 200);
     } else {
-      console.log("Please fill in all required fields.");
+        showAlert = true;
+        alertMessage = "Please fill all fields!";
     }
   }
 
@@ -68,5 +71,21 @@
             <!-- <button class="btn btn-success" on:click={handleSubmit}>Save</button> -->
             <button class="btn btn-outline btn-primary rounded-full hover:text-white w-32 on:click={handleSubmit}">Save</button>
         </div>
+        {#if showAlert}
+        <div role="alert" class="alert alert-warning mt-4">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg>
+            <span>{alertMessage}</span>
+        </div>
+        {/if}
     </div>
 </dialog>

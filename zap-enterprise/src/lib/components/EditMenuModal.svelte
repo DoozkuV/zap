cd6f9551e8092efcd82a46bc342ec6b345598fe6
Hannah Wang <43276816+hannah0wang@users.noreply.github.com>
2024-07-28 23:31:15 -0500

    edit menu modal

---
+++ zap-enterprise/src/lib/components/EditMenuModal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let isModalOpen: boolean;
+</script>
+
+<dialog class="modal" class:modal-open={isModalOpen}>
+    <div class="modal-box max-w-xl">
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" on:click={() => {
+            isModalOpen = false; 
+            setTimeout(() => {
+                selectedLang = "";
+                searchInput = "";
+            }, 200);
+        }}>✕</button>
+        <div class="flex-none pl-10">
+            <h1>Menu Category</h1>
+        </div>
+    </div>
+</dialog>

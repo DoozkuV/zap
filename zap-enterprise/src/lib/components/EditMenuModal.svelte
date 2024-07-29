d4654a4fa91a65753c41d5c52e7d39069c96bf53
Hannah Wang <43276816+hannah0wang@users.noreply.github.com>
2024-07-29 15:40:18 -0500

    add supabase to enterprise

--- zap-enterprise/src/lib/components/EditMenuModal.svelte
+++ zap-enterprise/src/lib/components/EditMenuModal.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     export let isModalOpen: boolean;
-    let input = ""
+    let categoryInput = ""
+    let itemNameInput = ""
+    let itemPriceInput = ""
+    let itemDescriptionInput = ""
 </script>
 
 <dialog class="modal" class:modal-open={isModalOpen}>
@@ -8,11 +11,34 @@
         <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" on:click={() => {
             isModalOpen = false; 
             setTimeout(() => {
-                input = "";
+                    categoryInput = ""
+                    itemNameInput = ""
+                    itemPriceInput = ""
+                    itemDescriptionInput = ""
             }, 200);
         }}>✕</button>
-        <div class="flex-none pl-10">
-            <h1>Menu Category</h1>
+        <div class="flex-none px-2">
+            <h1 class="text-left p-1 text-lg">Menu Category</h1>
+            <label class="input input-bordered w-full mb-4 flex items-center gap-2 hover:bg-gray-300 hover:border-gray-300 hover:text-black transition duration-500 ease-in-out">
+                <input type="text" class="grow text-base" placeholder="e.g. Chef Specials, Appetizers, Beverages" bind:value={categoryInput} />
+            </label>
+            <h1 class="text-left p-1 text-md">Item Name</h1>
+            <label class="input input-bordered w-full mb-4 flex items-center gap-2 hover:bg-gray-300 hover:border-gray-300 hover:text-black transition duration-500 ease-in-out">
+                <input type="text" class="grow text-base" placeholder="e.g. Caesar Salad, Spaghetti Bolognese, Tiramisu" bind:value={itemNameInput} />
+            </label>
+            <h1 class="text-left p-1 text-md">Item Price</h1>
+            <label class="input input-bordered w-full mb-4 flex items-center gap-2 hover:bg-gray-300 hover:border-gray-300 hover:text-black transition duration-500 ease-in-out">
+                <input type="number" class="grow text-base" placeholder="12.99" bind:value={itemPriceInput} />
+            </label>
+            <h1 class="text-left p-1 text-md">Item Description</h1>
+            <textarea 
+                class="textarea textarea-bordered w-full mb-4 text-base flex items-center gap-2 hover:bg-gray-300 hover:border-gray-300 hover:text-black transition duration-500 ease-in-out" 
+                placeholder="Describe your dish"
+                bind:value={itemDescriptionInput}></textarea>
+        </div>
+        <div class="flex justify-center mt-4">
+            <!-- <button class="btn btn-success" on:click={handleSubmit}>Save</button> -->
+            <button class="btn btn-outline btn-primary rounded-full hover:text-white w-32">Search</button>
         </div>
     </div>
 </dialog>
